5eaa8d635afb9fc8a0ed05f6511e7c10a77ae7b8
Murilo Rocha <murilorocha537@gmail.com>
2023-06-08 19:24:56 -0300

    deleted <p>

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,9 +6,7 @@ function Home() {
     <div className="Home">
       <header className="App-header">
         <img src={"https://i0.wp.com/imagensemoldes.com.br/wp-content/uploads/2020/05/Figura-Filhote-Pato-PNG.png?fit=760%2C549&ssl=1"} className="App-logo" alt="logo" />
-        <p>
-          Patos acima
-        </p>
+        
         <a
           className="App-link"
           href="https://pt.wikipedia.org/wiki/Pato"
