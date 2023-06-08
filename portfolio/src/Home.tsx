1fae46faf55bf30d5360417a14397ac360572831
Murilo Rocha <murilorocha537@gmail.com>
2023-06-08 19:00:31 -0300

    first commit react app

---
+++ portfolio/src/Home.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <img src={"https://i0.wp.com/imagensemoldes.com.br/wp-content/uploads/2020/05/Figura-Filhote-Pato-PNG.png?fit=760%2C549&ssl=1"} className="App-logo" alt="logo" />
+        <p>
+          Patos acima
+        </p>
+        <a
+          className="App-link"
+          href="https://pt.wikipedia.org/wiki/Pato"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Patos
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
